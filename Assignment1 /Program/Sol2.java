40fb3d11acde00dac5fea1267d669f464ec3d7d7
krushikbhandaricmaug26 <krushik.bhandari.cmaug26@gmail.com>
2026-08-19 12:02:52 +0530

    Add files via upload

---
+++ Assignment1 /Program/Sol2.java
@@ -0,0 +1,12 @@
+// Write a Java program that checks if a predefined number is negative using an if-else statement and displays the result.
+class Sol2 {
+    public static void main(String[] args) {
+        int number = -10;
+
+        if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is not negative.");
+        }
+    }
+}
