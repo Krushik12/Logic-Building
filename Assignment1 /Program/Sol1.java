4eadf19ddb597cf30630726583edab3b469f3ec2
krushikbhandaricmaug26 <krushik.bhandari.cmaug26@gmail.com>
2026-08-19 12:01:35 +0530

    Add files via upload

---
+++ Assignment1 /Program/Sol1.java
@@ -0,0 +1,18 @@
+//Write a Java program that checks if a predefined number is positive using an if-else statement and prints the appropriate message.
+class Sol1 {
+	public static void main(String[] args){
+		
+		int num = 30;
+		
+		if(num > 0) {
+			System.out.println("The is positive." );
+		
+		}
+		else{
+		System.out.println("The is not positive.");
+		}
+	
+	}
+
+
+}
