a3196fc0d1e8488337ebd74aefd130b3363232c6
krushik.bhandari_AC <krushik.bhandari.cmaug26@gmail.com>
2026-08-20 10:18:57 +0530

    Add files via upload

---
+++ Assignment2 /Program/Prob4.java
@@ -0,0 +1,24 @@
+//Identify the Values Of Uninitializeed variables
+
+class Prob4 {
+    public static void main(String[] args) {
+
+        byte a = 10;
+        short b = 100;
+        int c = 1000;
+        long d = 10000L;
+        float e = 10.5f;
+        double f = 20.55;
+        char g = 'A';
+        boolean h = true;
+
+        System.out.println("byte: " + a);
+        System.out.println("short: " + b);
+        System.out.println("int: " + c);
+        System.out.println("long: " + d);
+        System.out.println("float: " + e);
+        System.out.println("double: " + f);
+        System.out.println("char: " + g);
+        System.out.println("boolean: " + h);
+    }
+}
